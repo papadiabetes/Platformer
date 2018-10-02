eb819418ff7826d528a1fb897f628c89f2f1e695
papadiabetes <43767724+papadiabetes@users.noreply.github.com>
2018-10-02 11:24:09 +1000

    new platformer game
    added the player and sprite classes

---
+++ Jump boi/Jump boi/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+namespace Jump_boi
+{
+    class Player
+    {
+        Sprite playerSprite = new Sprite();
+
+        public Player()
+        {
+
+        }
+
+
+        public void Load(ContentManager content)
+        {
+            playerSprite.Load(content, "hero");
+        }
+
+
+        public void Update(float deltaTime)
+        {
+            playerSprite.Update(deltaTime);
+        }
+        public void Draw(SpriteBatch spriteBatch)
+
+        {
+            playerSprite.Draw(spriteBatch);
+        }
+
+    }
+}
